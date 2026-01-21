474cd17c5761218422507b9a86137c7a7e0a72b0
Tejun Heo <tj@kernel.org>
2026-01-21 12:46:45 -1000

    sched_ext: Implement cgroup sub-sched enabling and disabling
    The preceding changes implemented the framework to support cgroup
    sub-scheds and updated scheduling paths and kfuncs so that they have
    minimal but working support for sub-scheds. However, actual sub-sched
    enabling/disabling hasn't been implemented yet and all tasks stayed on
    scx_root.
    
    Implement cgroup sub-sched enabling and disabling to actually activate
    sub-scheds:
    
    - Both enable and disable operations bypass only the tasks in the subtree
      of the child being enabled or disabled to limit disruptions.
    
    - When enabling, all candidate tasks are first initialized for the child
      sched. Once that succeeds, the tasks are exited for the parent and then
      switched over to the child. This adds a bit of complication but
      guarantees that child scheduler failures are always contained.
    
    - Disabling works the same way in the other direction. However, when the
      parent may fail to initialize a task, disabling is propagated up to the
      parent. While this means that a parent sched fail due to a child sched
      event, the failure can only originate from the parent itself (its
      ops.init_task()). The only effect a malfunctioning child can have on the
      parent is attempting to move the tasks back to the parent.
    
    After this change, although not all the necessary mechanisms are in place
    yet, sub-scheds can take control of their tasks and schedule them.
    
    Signed-off-by: Tejun Heo <tj@kernel.org>

--- include/linux/sched/ext.h
+++ include/linux/sched/ext.h
@@ -87,6 +87,7 @@ enum scx_ent_flags {
 	SCX_TASK_QUEUED		= 1 << 0, /* on ext runqueue */
 	SCX_TASK_RESET_RUNNABLE_AT = 1 << 2, /* runnable_at should be reset */
 	SCX_TASK_DEQD_FOR_SLEEP	= 1 << 3, /* last dequeue was for SLEEP */
+	SCX_TASK_SUB_INIT	= 1 << 4, /* task being initialized for a sub sched */
 
 	SCX_TASK_STATE_SHIFT	= 8,	  /* bit 8 and 9 are used to carry scx_task_state */
 	SCX_TASK_STATE_BITS	= 2,
