e343f02fa3b16304c4ce1df417713e3ea1354526
Tejun Heo <tj@kernel.org>
2026-05-14 12:29:40 -1000

    bpf/arena: Add bpf_arena_map_kern_vm_start() and bpf_prog_arena()
    struct bpf_arena is opaque to callers outside arena.c. Add two helpers
    for struct_ops subsystems that need to reach into an arena:
    
      bpf_arena_map_kern_vm_start(struct bpf_map *map)
        returns @map's kern_vm_start. A sched_ext follow-up needs this
        to translate kern_va <-> uaddr.
    
      bpf_prog_arena(struct bpf_prog *prog)
        returns the bpf_map of the arena referenced by @prog (NULL if
        @prog references no arena). The verifier enforces at most one
        arena per program. Used by struct_ops callers that auto-discover
        an arena from a member prog and need to take a map reference.
    
    Suggested-by: Kumar Kartikeya Dwivedi <memxor@gmail.com>
    Signed-off-by: Tejun Heo <tj@kernel.org>

--- include/linux/bpf.h
+++ include/linux/bpf.h
@@ -617,6 +617,8 @@ void bpf_rb_root_free(const struct btf_field *field, void *rb_root,
 		      struct bpf_spin_lock *spin_lock);
 u64 bpf_arena_get_kern_vm_start(struct bpf_arena *arena);
 u64 bpf_arena_get_user_vm_start(struct bpf_arena *arena);
+u64 bpf_arena_map_kern_vm_start(struct bpf_map *map);
+struct bpf_map *bpf_prog_arena(struct bpf_prog *prog);
 bool bpf_arena_handle_page_fault(unsigned long addr, bool is_write, unsigned long fault_ip);
 int bpf_obj_name_cpy(char *dst, const char *src, unsigned int size);
 
