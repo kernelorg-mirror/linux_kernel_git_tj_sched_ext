51c109a1e5522edaca851f0530be9ed1a9cb2235
Tejun Heo <tj@kernel.org>
2026-07-09 12:41:39 -1000

    sched_ext: Add scx_skip_subtree_pre()
    Factor the sibling/ancestor portion of scx_next_descendant_pre() out as
    scx_skip_subtree_pre(), a pre-order walk primitive that skips @pos's
    subtree, and call it from scx_next_descendant_pre(). Same locking rules as
    the existing primitive.
    
    Used in a follow-up to fast-skip subtrees that have nothing to do during a
    descendant walk.
    
    Signed-off-by: Tejun Heo <tj@kernel.org>

--- kernel/sched/ext/sub.h
+++ kernel/sched/ext/sub.h
@@ -15,6 +15,7 @@
 
 #ifdef CONFIG_EXT_SUB_SCHED
 
+struct scx_sched *scx_skip_subtree_pre(struct scx_sched *pos, struct scx_sched *root);
 struct scx_sched *scx_next_descendant_pre(struct scx_sched *pos, struct scx_sched *root);
 void scx_set_task_sched(struct task_struct *p, struct scx_sched *sch);
 struct cgroup *sch_cgroup(struct scx_sched *sch);
@@ -30,6 +31,7 @@ s32 scx_alloc_pshards(struct scx_sched *sch);
 #else	/* CONFIG_EXT_SUB_SCHED */
 
 static inline struct scx_sched *scx_next_descendant_pre(struct scx_sched *pos, struct scx_sched *root) { return pos ? NULL : root; }
+static inline struct scx_sched *scx_skip_subtree_pre(struct scx_sched *pos, struct scx_sched *root) { return NULL; }
 static inline void scx_set_task_sched(struct task_struct *p, struct scx_sched *sch) {}
 static inline struct cgroup *sch_cgroup(struct scx_sched *sch) { return NULL; }
 static inline void set_cgroup_sched(struct cgroup *cgrp, struct scx_sched *sch) {}
